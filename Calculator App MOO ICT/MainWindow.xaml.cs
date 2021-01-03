d8e456c1907fc9970b9b2de9c61e67ec6ef9518d
Anhar Ali <67914277+mooict@users.noreply.github.com>
2021-01-03 18:38:28 +0000

    Add files via upload

---
+++ Calculator App MOO ICT/MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Calculator_App_MOO_ICT
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        int firstNumber;
+        int secondNumber;
+        int total;
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CalculateAdd(object sender, RoutedEventArgs e)
+        {
+            firstNumber = Convert.ToInt32(numA.Text);
+            secondNumber = Convert.ToInt32(numB.Text);
+
+            total = firstNumber + secondNumber;
+
+            result.Content = total.ToString();
+        }
+
+        private void calculateSubtract(object sender, RoutedEventArgs e)
+        {
+            firstNumber = Convert.ToInt32(numA.Text);
+            secondNumber = Convert.ToInt32(numB.Text);
+
+            total = firstNumber - secondNumber;
+
+            result.Content = total.ToString();
+        }
+
+        private void CalculateMultiply(object sender, RoutedEventArgs e)
+        {
+            firstNumber = Convert.ToInt32(numA.Text);
+            secondNumber = Convert.ToInt32(numB.Text);
+
+            total = firstNumber * secondNumber;
+
+            result.Content = total.ToString();
+        }
+
+        private void CalculateDivide(object sender, RoutedEventArgs e)
+        {
+            firstNumber = Convert.ToInt32(numA.Text);
+            secondNumber = Convert.ToInt32(numB.Text);
+
+            double total = (double)firstNumber / secondNumber;
+
+            result.Content = total.ToString("#.##");
+
+        }
+    }
+}
